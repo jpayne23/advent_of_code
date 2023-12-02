bf1293c716822db79900e548b14200fdb1ea4bf4
Joe Payne <joe@hometree.co.uk>
2023-12-02 18:17:07 +0000

    UPDATE 2022 main file. WIP but probably won't get back to it now

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -3,9 +3,9 @@ mod day10;
 mod day5;
 
 fn main() {
-    // day1::part2("data/day1/input.txt");
+    day1::part2("data/day1/input.txt");
 
-    // day5::part1("data/day5/test.txt")
+    day5::part1("data/day5/test.txt");
 
     day10::part1("data/day10/test.txt", [20, 60, 100, 140, 180, 220].to_vec());
 }
