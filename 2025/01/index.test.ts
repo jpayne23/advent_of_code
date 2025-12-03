8b6f1968bde08870c3393e286842fbd6ae05ac96
Joe Payne <joe@hometree.co.uk>
2025-12-03 18:38:31 +0000

    2025 D1 P1 done

--- 2025/01/index.test.ts
+++ 2025/01/index.test.ts
@@ -4,14 +4,14 @@ describe("01", () => {
     describe("01", () => {
         it("should work", async () => {
             const result = await main("1", true);
-            expect(result).toBe(11);
+            expect(result).toBe(3);
         });
     });
 
     describe("02", () => {
         it("should work", async () => {
             const result = await main("2", true);
-            expect(result).toBe(31);
+            expect(result).toBe(6);
         });
     });
 });
--- 2025/01/index.ts
+++ 2025/01/index.ts
@@ -1,30 +1,93 @@
 import { readFile } from "../util/file";
 
-const separator = "  ";
-
 const p1 = async (filePath: string) => {
     const result = await readFile(filePath);
+    const initialPosition = 50;
 
-    const left = result.map(r => parseInt(r.split(separator)[0].trim())).sort();
-    const right = result.map(r => parseInt(r.split(separator)[1].trim())).sort();
+    const instructions = result.map(r => {
+        if (r.startsWith("R")) {
+            return Number(r.slice(1).trim());
+        }
 
-    const answer = left.reduce((acc, r, i) => {
-        return (acc += Math.abs(r - right[i]));
-    }, 0);
+        return Number(r.slice(1).trim()) * -1;
+    });
+
+    let answer = 0;
+
+    instructions.reduce((acc, r) => {
+        acc = acc + r;
+
+        if (acc < 0) {
+            acc = 100 * Math.floor(Math.abs(acc) / 100) + acc;
+        }
+
+        if (acc > 99) {
+            acc = acc - 100 * Math.floor(acc / 100);
+        }
+
+        if (acc === 0) {
+            answer = answer + 1;
+        }
+
+        return acc;
+    }, initialPosition);
 
     return answer;
 };
 
 const p2 = async (filePath: string) => {
     const result = await readFile(filePath);
+    const initialPosition = 50;
 
-    const left = result.map(r => parseInt(r.split(separator)[0].trim())).sort();
-    const right = result.map(r => parseInt(r.split(separator)[1].trim())).sort();
+    const instructions = result.map(r => {
+        if (r.startsWith("R")) {
+            return Number(r.slice(1).trim());
+        }
 
-    const answer = left.reduce((acc, r, i) => {
-        return (acc += r * right.filter(v => r === v).length);
-    }, 0);
+        return Number(r.slice(1).trim()) * -1;
+    });
 
+    let answer = 0;
+
+    instructions.reduce((acc, r) => {
+        let prev = acc;
+        acc = acc + r;
+
+        if (acc < 0) {
+            const loops = Math.floor(Math.abs(acc) / 100) + 1;
+            acc = 100 * loops + acc;
+
+            answer = answer + loops;
+
+            if (prev === 0) {
+                answer = answer - 1;
+            }
+        }
+
+        if (acc > 99) {
+            const loops = Math.floor(Math.abs(acc) / 100);
+            const pre = acc;
+            acc = acc - 100 * loops;
+
+            answer = answer + loops;
+
+            if (pre % 100 === 0) {
+                answer = answer - 1;
+            }
+
+            // if (prev === 0) {
+            //     answer = answer - 1;
+            // }
+        }
+
+        if (acc === 0) {
+            answer = answer + 1;
+        }
+
+        return acc;
+    }, initialPosition);
+
+    // Somewhere between 6908 and 7042
     return answer;
 };
 
